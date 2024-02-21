e944ee7e652b26a4e15a7e2dd1a0ff9e0d8d1f89
Krimal Patel <pakrimal@sheridancollege.ca>
2024-02-21 14:10:29 -0500

    Second Commit - New Branch created and added comments

---
+++ src/midtermexam_versiona_extensioncode/krimal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ * Name: Krimal Patel
+ * Date: 21-02-2024
+ * MidTerm Exam Part B 
+ * @author krima
+ */
+public class krimal {
+    
+}
